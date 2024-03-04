ef62179ad6187f6b2228e07599d9e430a36ec8ee
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-03-03 22:01:03 -0800

    Add files via upload
    3rd challenge of the day.............

---
+++ main.ts
@@ -0,0 +1,50 @@
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+let num = 7;
+let num1 = 1;
+console.log(num + num1)
+
+let num2 = 12;
+let num3 = 4;
+console.log(num2 -num3)
+
+
+let num4 = 2;
+let num5 = 4;
+console.log(num4 * num5)
+
+
+let num6 = 32;
+let num7 = 4;
+console.log(num6 / num7)
+
+
+
+// Question 8: You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log("-".repeat(80));
+let num8 = 5;
+let num9 = 3;
+console.log(5 + 3);
+console.log("-".repeat(80));
+let num10 = 12;
+let num11 = 4;
+console.log(12 - 4);
+console.log("-".repeat(80));
+let num12 = 2;
+let num13 = 4;
+console.log(2 * 4);
+console.log("-".repeat(80));
+let num14 = 64;
+let num15 = 8;
+console.log(64 / 8);
+
+
+
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message
+
+let favoriteNumber: number = 90;
+console.log(`My favoriteNumberis ${favoriteNumber}`)
+
+
